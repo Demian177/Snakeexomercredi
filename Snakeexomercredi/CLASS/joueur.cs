0c3c024c1f8ddafae332ebef458417b0000bcff0
Demian177 <damiendesaintes@gmail.com>
2022-10-26 16:48:13 +0200

    set direction

--- Snakeexomercredi/CLASS/joueur.cs
+++ Snakeexomercredi/CLASS/joueur.cs
@@ -6,13 +6,55 @@ using System.Threading.Tasks;
 
 namespace Snakeexomercredi.CLASS
 {
-    public class Joueur
+    public class Joueur:Map
     {
+        ConsoleKeyInfo clav = new ConsoleKeyInfo();
+        char key = 'z';
+        string direction = "haut";
+        List<position> Snake;
+        public int x;
+        public int y;
         public Joueur()
         {
+            x = largeur/2;
+            y = longueur/2;
+            List<position> Snake = new List<position>(); // donnez une nouvelle liste
+            Snake.Add(new position(x, y)); // add nouvelle position dans la liste vide elle recupere la tete du serpent 
         }
-        int x;
-        int y;
-        int score;
+        public override void initmap()
+        {
+            foreach (position position in Snake)
+            {
+                Console.SetCursorPosition(position.x, position.y); // recuperer le joueur et l'afficher
+                Console.WriteLine("O"); // choisir forme du perso
+            }
+        }
+        public void clavier()
+        {
+            if (Console.KeyAvailable)
+            {
+                clav = Console.ReadKey(true);
+                key = clav.KeyChar;
+            }
+        }
+        private void directions()
+        {
+            if (key=='z' && direction != "bas")//haut
+            {
+                direction = "haut";
+            }
+            else if (key == 'q' && direction != "droite")
+            {
+                direction = "gauche";
+            }
+            else if (key == 'd' && direction != "gauche")
+            {
+                direction = "droite";
+            }
+            else if (key == 's' && direction != "haut")
+            {
+                direction = "bas";
+            }
+        }
     }
 }
